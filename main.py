bad3fc03bb8ab24e9b6c95f639fce1b929248376
Misha <mishagrigoryan@gmail.com>
2023-12-16 23:14:37 -0500

    code

---
+++ main.py
@@ -0,0 +1,23 @@
+import config
+
+
+import requests
+import base64
+
+CLIENT_ID = config.CLIENT_ID
+CLIENT_SECRET = config.CLIENT_SECRET
+
+def get_access_token(client_id, client_secret):
+    client_creds = f"{client_id}:{client_secret}"
+    client_creds_b64 = base64.b64encode(client_creds.encode()).decode()
+
+    token_url = "https://accounts.spotify.com/api/token"
+    token_data = {"grant_type": "client_credentials"}
+    token_headers = {"Authorization": f"Basic {client_creds_b64}"}
+
+    r = requests.post(token_url, data=token_data, headers=token_headers)
+    token_response_data = r.json()
+    return token_response_data["access_token"]
+
+access_token = get_access_token(CLIENT_ID, CLIENT_SECRET)
+print(access_token)
